adeb2502693ee0a699ba0fdade071c38b4f960b2
Erenerbyc <76466488+CounTPaSa@users.noreply.github.com>
2023-11-01 13:51:33 +0300

    Add files via upload
    Lutfen kodlari cozumlemeye calisin. Copy-Paste yapmayin.

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merboyac <muheren2004@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 23:41:09 by merboyac          #+#    #+#             */
+/*   Updated: 2023/10/18 00:08:07 by merboyac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		tmp = ft_lstlast(*lst);
+		tmp -> next = new;
+	}
+}
---
+++ libft/ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merboyac <muheren2004@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 00:45:05 by merboyac          #+#    #+#             */
+/*   Updated: 2023/10/18 01:07:32 by merboyac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst)
+		return ;
+	del(lst ->content);
+	free(lst);
+}
